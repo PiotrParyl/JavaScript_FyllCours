89f9ecc9a49049a3b0e99e8cc890f12d39822790
piotr <piotrekzrydek@gmial.com>
2022-08-23 17:30:14 +0200

    Ooowtight, first houer of JS is behind me, so far so good it looks preeti nice, i like it <33

--- test.js
+++ test.js
@@ -1,1 +1,99 @@
+
+// first comment
 console.log("Hello JS")
+
+/*
+Multimple ine comment, veru usfull
+*/
+
+/* Data types:
+undefined, null, boolean, string, symbol, number, and object (meny of thous are in python nice :)) )
+*/
+
+var myname = "pitrlolo" // when use var, it will be use whone my program 
+
+myAge = 24
+
+let yourname = "bobo" // this one , is use in moment (i gess :/)
+
+const myweight = 80 // ;) this one will never change like ccrotka in python
+
+var a;
+var b=7;
+
+
+a = 69;
+
+// declaration ( so it looks like in JS we have to first declerate varable and then we can add some value to dhis varable (string,int, ore smth))
+
+var twojaStara;
+var wagaTwojejStarej;
+var zarobkiTwojejStarej;
+
+// and now we can put some numbers to this vatable 
+
+twojaStara = 'Bronislawa';
+wagaTwojejStarej = 69;
+zarobkiTwojejStarej = 9000;
+
+console.log(twojaStara, wagaTwojejStarej, zarobkiTwojejStarej)
+
+// ok naw dude from cours teling me how to add two numbers xDDDD lest skip this one 
+
+var xD = 10 + 10; 
+
+// so in js when we wont make ouer number minus one mw have to use "--" operator, in python we use "-=" so far so good :))) 
+
+// some oparation with decimals, so il looks like we have to have dwo decimal numbers and then we can add thows two. (id dont remember how it lok like python i go check xD )
+// okeeee in python we can add int to float nice 
+
+var numnumnumberssss = 2.0 + 3.3;
+
+console.log(numnumnumberssss)
+
+var whatnow = 2.2 + 5;
+
+console.log(whatnow)
+ // yo looks like we can allsow add float to int yo yo eminem nice. 
+
+// right now he teling about strings ... 
+
+// finaly something intresting with string, how to count leaters in word.
+
+var password = 'qwerty1234'
+
+console.log(password.length) // this functionality i use when i have to setup passwort for user. i python we use len().
+
+
+// In js we cant change one letter in string, we have to change whole word. look:
+
+var string = "Jello World";
+
+string = 'Hello World';
+
+console.log(string);
+
+// finally some thin intresting, FUNCTIONS !!!!
+
+function firsFunction (myFriend, whatHeDo, whereHeDoThat) {
+
+    var name = myFriend;
+    var whatdo  = whatHeDo;
+    var where = whereHeDoThat;
+
+    var resoult = `The ${name} is ${whatdo} to the ${where}`; // in python this f() function looks little better hihihihi
+
+    return resoult;
+}
+
+console.log(firsFunction('Nowak','runing','shopcenter'));
+
+
+
+
+
+
+ 
+
+
+
